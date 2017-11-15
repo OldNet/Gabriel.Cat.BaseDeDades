3594e944270058f9e6fe116a774808cd6b2d2728
Gabriel <tetradog@gmail.com>
2017-11-16 00:57:24 +0100

    commit

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BaseDeDatos")]
+[assembly: AssemblyTitle("Gabriel.Cat.BaseDeDades")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaseDeDatos")]
-[assembly: AssemblyCopyright("Copyright 2015")]
+[assembly: AssemblyProduct("Gabriel.Cat.BaseDeDades")]
+[assembly: AssemblyCopyright("Copyright 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
